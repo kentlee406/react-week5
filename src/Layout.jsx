87474994542f732cfeea313f4b55876a713e6e26
kentlee406 <157895655+kentlee406@users.noreply.github.com>
2026-01-24 17:11:33 +0800

    產品列表頁

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,20 @@
 import { NavLink, Outlet } from "react-router";
+import "./scss/all.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function Layout() {
   return (
-    <div>
-      <nav>
-        <NavLink to="">首頁</NavLink>|<NavLink to="/products">產品</NavLink>
-      </nav>
+    <div className="container">
+      <header>
+        <h1>TC 3C電子商務</h1>
+        <nav>
+          <NavLink to="">首頁</NavLink>|<NavLink to="/products">產品</NavLink>
+        </nav>
+      </header>
       <Outlet />
-      <footer>表尾</footer>
+      <footer>
+        <p>TC電腦有限公司 版權所有 轉載必究</p>
+      </footer>
     </div>
   );
 }
--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,4 +1,63 @@
+import axios from "axios";
+import "bootstrap";
+import { useState, useEffect } from "react";
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
 function Products() {
-  return <h2>產品列表</h2>;
+  const [products, setProducts] = useState([]);
+  const getProductData = async () => {
+    try {
+      const response = await axios.get(
+        `${API_BASE}/api/${API_PATH}/products/all`,
+      );
+      setProducts(response.data.products);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  return (
+    <>
+      <h2>產品列表</h2>
+      <div className="cards">
+        <div className="row g-4">
+          {products && products.length > 0
+            ? products.map((product) => {
+                return (
+                  <div className="col-4">
+                    <div className="card">
+                      <img
+                        src={product.imageUrl}
+                        class="card-img-top"
+                        alt={product.title}
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">
+                          {product.title}
+                          <span className="badge bg-primary ms-2">
+                            {product.category}
+                          </span>
+                        </h5>
+                        <p className="card-text">{product.content}</p>
+                        <div className="d-flex">
+                          <p className="card-text text-secondary">
+                            <del>{product.origin_price}</del>
+                          </p>
+                          &nbsp;元 / {product.price} 元
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            : "目前尚無產品資料"}
+        </div>
+      </div>
+    </>
+  );
 }
 export default Products;
